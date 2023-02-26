45fe92cbe8a3f9b5a497b88e8c68ee5506896dce
factism001 <factism001@gmail.com>
2023-02-26 08:08:46 +0100

    update

--- sort.h
+++ sort.h
@@ -8,7 +8,7 @@
 #define UP 0
 #define DOWN 1
 
-/** 
+/**
  * enum bool - Enumeration of boolean values
  */
 typedef enum bool
@@ -44,7 +44,15 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 
 #endif
